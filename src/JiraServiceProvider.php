94531bcc159b1c697d8feea57aafa656cdde37ec
Ivan Timofeev <nighthtr@gmail.com>
2022-12-18 19:38:18 +0700

    service provider

---
+++ src/JiraServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nighthtr\Jira;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class JiraServiceProvider
+ * @package Nighthtr\Jira
+ */
+class JiraServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
